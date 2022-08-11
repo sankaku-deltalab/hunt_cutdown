3efde70899b99e5fffebd31ffe6ca87a8473ea99
Sankaku <sankaku_dlt.45631@outlook.jp>
2022-08-11 09:51:13 +0000

    Add seed data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,42 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+defmodule HuntCutdown.SeedModule do
+  alias HuntCutdown.Equipments.{
+    WeaponCategories,
+    Weapons,
+    WeaponAmmos,
+    ToolCategories,
+    Tools,
+    ConsumableCategories,
+    Consumables
+  }
+
+  def assign() do
+    path_base = "priv/repo/seed_data"
+
+    data_pairs = [
+      {%WeaponCategories{}, "weapon_categories.csv"},
+      {%Weapons{}, "weapons.csv"},
+      {%WeaponAmmos{}, "weapon_ammos.csv"},
+      {%ToolCategories{}, "tool_categories.csv"},
+      {%Tools{}, "tools.csv"},
+      {%ConsumableCategories{}, "consumable_categories.csv"},
+      {%Consumables{}, "consumables.csv"}
+    ]
+
+    for {struct, path_rel} <- data_pairs do
+      path = Path.join(path_base, path_rel)
+      df = Explorer.DataFrame.from_csv!(path)
+
+      for row <- Explorer.DataFrame.to_rows(df) do
+        struct
+        |> struct.__struct__.changeset(row)
+        |> HuntCutdown.Repo.insert!()
+      end
+    end
+  end
+end
+
+HuntCutdown.SeedModule.assign()
