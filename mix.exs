769a9d6890e6459a2a1bde80acbbeb9c613ebfef
Sankaku <sankaku_dlt.45631@outlook.jp>
2022-08-13 05:51:34 +0000

    Add dialyzer and execute it at ci

--- mix.exs
+++ mix.exs
@@ -49,7 +49,8 @@ defmodule HuntCutdown.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:explorer, "~> 0.2.0"}
+      {:explorer, "~> 0.2.0"},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
     ]
   end
 
