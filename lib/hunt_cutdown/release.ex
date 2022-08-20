5e52e43d56aabd3492a9dc6ad75a86102bd246a7
Sankaku <sankaku_dlt.45631@outlook.jp>
2022-08-20 08:40:09 +0000

    Run seed setup at release

--- lib/hunt_cutdown/release.ex
+++ lib/hunt_cutdown/release.ex
@@ -11,6 +11,8 @@ defmodule HuntCutdown.Release do
     for repo <- repos() do
       {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
     end
+
+    HuntCutdown.SeedModule.Setup.run()
   end
 
   def rollback(repo, version) do
