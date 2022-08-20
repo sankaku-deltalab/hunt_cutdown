b4b4849edda898e6be1bdd43c0af5b478c327de6
Sankaku <sankaku_dlt.45631@outlook.jp>
2022-08-20 09:01:34 +0000

    Add explorer config for seed in deploy

--- config/config.exs
+++ config/config.exs
@@ -61,3 +61,5 @@ config :tailwind,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
+
+config :explorer, :default_backend, Explorer.PolarsBackend
