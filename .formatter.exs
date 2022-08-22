19524d219a37eecb0158537d3ad609a50d96fcb0
Sankaku <sankaku_dlt.45631@outlook.jp>
2022-08-22 15:24:04 +0000

    Fix alert view

--- .formatter.exs
+++ .formatter.exs
@@ -1,6 +1,6 @@
 [
   import_deps: [:ecto, :phoenix],
   plugins: [Phoenix.LiveView.HTMLFormatter],
-  inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{heex,ex,exs}"],
+  inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*^(live.html).{heex,ex,exs}"],
   subdirectories: ["priv/*/migrations"]
 ]
