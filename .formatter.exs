5d72d0af6d9996429f0fce7f5948bb33abb17e1e
Sankaku <sankaku_dlt.45631@outlook.jp>
2022-08-23 13:23:31 +0000

    Fix formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,6 +1,6 @@
 [
   import_deps: [:ecto, :phoenix],
   plugins: [Phoenix.LiveView.HTMLFormatter],
-  inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*^(live.html).{heex,ex,exs}"],
-  subdirectories: ["priv/*/migrations"]
+  inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{heex,ex,exs}"],
+  subdirectories: ["priv/*/migrations", "lib/hunt_cutdown/templates/layout"]
 ]
---
+++ lib/hunt_cutdown_web/templates/layout/.formatter.exs
@@ -0,0 +1,6 @@
+[
+  import_deps: [:ecto, :phoenix],
+  plugins: [Phoenix.LiveView.HTMLFormatter],
+  # inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{heex,ex,exs}"],
+  inputs: []
+]
