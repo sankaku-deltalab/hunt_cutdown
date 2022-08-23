098b3f1a07b6e36b2f94f6b517b8112eeac436a2
Sankaku <sankaku_dlt.45631@outlook.jp>
2022-08-23 13:32:00 +0000

    Change selector_button
    It takes event and json_payload.

--- lib/hunt_cutdown_web/live/equipment_live/component/selector_button.ex
+++ lib/hunt_cutdown_web/live/equipment_live/component/selector_button.ex
@@ -12,13 +12,19 @@ defmodule HuntCutdownWeb.EquipmentLive.Components.SelectorButton do
   @impl true
   def render(
         %{
+          event: event,
+          json_payload: json_payload,
           enabled: enabled
         } = assigns
-      ) do
+      )
+      when event |> is_bitstring() and json_payload |> is_bitstring() and enabled |> is_boolean() do
     ~H"""
-    <button class={"btn btn-xs capitalize justify-start #{if not enabled do
-          "btn-ghost"
-        end} w-full"} disabled={not enabled}>
+    <button
+      class={"btn btn-xs capitalize justify-start #{if not enabled, do: "btn-ghost"} w-full"}
+      disabled={not enabled}
+      phx-click={if enabled, do: event}
+      phx-value-json_payload={json_payload}
+    >
       <%= if enabled do %>
         <%= render_slot(@inner_block) %>
       <% else %>
